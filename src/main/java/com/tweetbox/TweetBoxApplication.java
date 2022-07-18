be57025704be3e0bc30de0807cef5a561d65b6ce
hanalen <jinsub2001@naver.com>
2022-07-18 23:55:03 +0900

    first commit - 프로젝트 생성
    야호

---
+++ src/main/java/com/tweetbox/TweetBoxApplication.java
@@ -0,0 +1,13 @@
+package com.tweetbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TweetBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tweetbox/TweetBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tweetbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
