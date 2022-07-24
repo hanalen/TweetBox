1c8acb7b2f89b556fab220ac601343e0a76d09cb
hanalen <jinsub2001@naver.com>
2022-07-24 23:01:54 +0900

    ProgressStatus 코틀린->자바 파일 변경

---
+++ src/main/java/com/tweetbox/progress/entities/ProgressStatus.java
@@ -0,0 +1,12 @@
+package com.tweetbox.progress.entities;
+
+public enum ProgressStatus {
+    NONE,
+    UPLOAD_START,
+    UPLOAD_END,
+    UNZIP_START,
+    UNZIP_END,
+    READ_START,
+    READ_END,
+    FAIL
+}
--- src/main/java/com/tweetbox/progress/entities/ProgressStatus.kt
+++
@@ -1,9 +0,0 @@
-package com.tweetbox.progress.entities
-
-enum class ProgressStatus {
-    NONE,
-    START,
-    ING,
-    END,
-    FAIL
-}
