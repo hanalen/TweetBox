f02d0cc2b6ded532af1c0d415769147aaf6bc8fe
hanalen <jinsub2001@naver.com>
2022-09-15 22:06:58 +0900

    스프링 시큐리티 추가

---
+++ src/main/java/com/tweetbox/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.tweetbox;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  @Override
+  protected void configure(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.httpBasic().disable();//모든 요청에 대해 로그인 페이지 리다이렉트 시키는 기능 해제 
+  }
+}
