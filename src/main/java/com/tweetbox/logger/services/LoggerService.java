cbd1e5a5888f89c75465e5d2f1316db5e428244d
hanalen <jinsub2001@naver.com>
2022-09-17 02:49:45 +0900

    로거 코드 컨벤션 적용

--- src/main/java/com/tweetbox/logger/services/LoggerService.java
+++ src/main/java/com/tweetbox/logger/services/LoggerService.java
@@ -15,51 +15,56 @@ import java.util.Date;
 
 @Component
 public class LoggerService implements HandlerInterceptor {
-    private final Logger logger = LoggerFactory.getLogger(LoggerService.class);
+  private final Logger logger = LoggerFactory.getLogger(LoggerService.class);
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-            throws Exception {
-        request.setAttribute("start", new Date());
-        return true;
-    }
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+          throws Exception {
+    request.setAttribute("start", new Date());
+    return true;
+  }
 
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-                           @Nullable ModelAndView modelAndView) throws Exception {
-    }
+  @Override
+  public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                         @Nullable ModelAndView modelAndView) throws Exception {
+  }
 
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
-                                @Nullable Exception ex) throws Exception {
-        Date startTime = (Date) request.getAttribute("start");
-        Date endTime = new Date();
-        Double diff = Double.valueOf(endTime.getTime() - startTime.getTime());
-        Double diffSec = diff / 1000;
-        String requestUrl = request.getRequestURI();
-        String method = request.getMethod();
-        String str = String.format("%s %s %f s", method.toUpperCase(), requestUrl, diffSec);
-        logger.info(str);
-    }
+  @Override
+  public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+                              @Nullable Exception ex) throws Exception {
+    Date startTime = (Date) request.getAttribute("start");
+    Date endTime = new Date();
+    Double diff = Double.valueOf(endTime.getTime() - startTime.getTime());
+    Double diffSec = diff / 1000;
+    String requestUrl = request.getRequestURI();
+    String method = request.getMethod();
+    String str = String.format("%s %s %f s", method.toUpperCase(), requestUrl, diffSec);
+    logger.info(str);
+  }
 
-    public void Debug(String msg) {
-        this.logger.debug(msg);
-    }
+  public void Debug(String msg) {
+    this.logger.debug(msg);
+  }
 
-    public void Info(String msg) {
-        this.logger.info(msg);
-    }
+  public void Info(String msg) {
+    this.logger.info(msg);
+  }
 
-    public void Trace(String msg) {
-        this.logger.trace(msg);
-    }
+  public void Info(String format, Object arg) {
+    this.logger.info(format, arg);
+  }
 
-    public void Warn(String msg) {
-        this.logger.warn(msg);
-    }
+  public void Trace(String msg) {
+    this.logger.trace(msg);
+  }
 
-    public void Error(String msg) {
-        this.logger.error(msg);
-    }
+  public void Warn(String msg) {
+    this.logger.warn(msg);
+
+  }
+
+  public void Error(String msg) {
+    this.logger.error(msg);
+  }
 
 }
