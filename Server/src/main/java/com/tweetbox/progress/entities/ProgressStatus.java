a41f45b2af5c1edafe5ef45df3133d6b6f2ad10a
hanalen <jinsub2001@naver.com>
2022-09-18 01:39:55 +0900

    ProgressStatus 추가

--- Server/src/main/java/com/tweetbox/progress/entities/ProgressStatus.java
+++ Server/src/main/java/com/tweetbox/progress/entities/ProgressStatus.java
@@ -1,13 +1,15 @@
 package com.tweetbox.progress.entities;
 
 public enum ProgressStatus {
-    NONE,
-    UPLOAD_START,
-    UPLOAD_END,
-    UNZIP_START,
-    UNZIP_END,
-    FILE_CORRECT,
-    READ_START,
-    READ_END,
-    FAIL
+  NONE,
+  UPLOAD_START,
+  UPLOAD_END,
+  UNZIP_START,
+  UNZIP_END,
+  FILE_CORRECT,
+  READ_START,
+  READ_END,
+  INSERT_START,
+  INSERT_END,
+  FAIL
 }
